025ecc0029be97b0c0ada25b7e0833bdcfbdccd4
Gwak <thekinggun@gmail.com>
2016-04-24 19:17:56 +0900

    ProudNet Setting

---
+++ Assets/Scripts/GameClient.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Nettention.Proud;
+
+public class GameClient : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
